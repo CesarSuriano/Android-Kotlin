1254ff03c4ad0070cbdbf8f50cd428cae89b9ee1
César Augusto <cesaraugustokg6@hotmail.com>
2017-05-21 01:20:13 -0300

    Abrindo tela

--- Testes/app/src/main/java/com/example/cesar/testes/MainActivity.kt
+++ Testes/app/src/main/java/com/example/cesar/testes/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.cesar.testes
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +13,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val botao = findViewById(R.id.botao) as Button
+        val botao2 = findViewById(R.id.botao2) as Button
+        val texto = findViewById(R.id.hello) as TextView
+
+        botao.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                texto.setText("Teste")
+            }
+        })
+
+        botao2.setOnClickListener { view -> texto.setText("Teste 2") }
 
     }
 }
